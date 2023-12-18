f281ad9e516f9a0549d4008c05126b1a4659f115
saif-ibne-ali <si87868@gmail.com>
2023-12-18 09:53:37 +0600

    delete and add operation . and gitignore update

--- lib/firebase_student_app.dart
+++ lib/firebase_student_app.dart
@@ -12,6 +12,9 @@ class _StudentListState extends State<StudentList> {
   FirebaseFirestore db = FirebaseFirestore.instance;
   late CollectionReference studentCollectionRef = db.collection('students');
   List<Student> studentList = [];
+  final TextEditingController nameTEController = TextEditingController();
+  final TextEditingController idTEController = TextEditingController();
+  final TextEditingController fieldTEController = TextEditingController();
 
   Future<void> getStudentData() async {
     final QuerySnapshot result = await studentCollectionRef.get();
@@ -34,18 +37,13 @@ class _StudentListState extends State<StudentList> {
         title: const Text('Student List'),
       ),
       floatingActionButton: FloatingActionButton(
-          child: const Icon(Icons.add),
-          onPressed: () {
-            studentCollectionRef.add({
-              'name': 'Zahid',
-              'id': '17151079',
-              'field': 'CSE',
-            });
-          }),
+        onPressed: showAddStudentBottomSheet,
+        child: const Icon(Icons.add),
+      ),
       body: RefreshIndicator(
         onRefresh: getStudentData,
         child: StreamBuilder(
-            stream: studentCollectionRef.snapshots(),
+            stream: studentCollectionRef.orderBy('id').snapshots(),
             builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
               if (snapshot.connectionState == ConnectionState.waiting) {
                 return const Center(
@@ -61,9 +59,11 @@ class _StudentListState extends State<StudentList> {
                 studentList.clear();
                 for (QueryDocumentSnapshot element in snapshot.data!.docs) {
                   Student student = Student(
-                      name: element.get('name'),
-                      id: element.get('id'),
-                      field: element.get('field'));
+                    name: element.get('name'),
+                    id: element.get('id'),
+                    field: element.get('field'),
+                    uniqueId: element.id,
+                  );
                   studentList.add(student);
                 }
                 return ListView.builder(
@@ -74,8 +74,36 @@ class _StudentListState extends State<StudentList> {
                           child: Icon(Icons.person),
                         ),
                         title: Text(studentList[index].name),
-                        subtitle: Text(studentList[index].id),
-                        trailing: Text(studentList[index].field),
+                        subtitle: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(studentList[index].id),
+                              Text(studentList[index].field),
+                            ]),
+                        trailing: Wrap(
+                          children: [
+                            IconButton(
+                              onPressed: () {
+                                studentCollectionRef
+                                    .doc(studentList[index].uniqueId)
+                                    .update({
+                                  'name': 'Rahi',
+                                  'id': '17151039',
+                                  'field': 'CSE'
+                                });
+                              },
+                              icon: const Icon(Icons.edit),
+                            ),
+                            IconButton(
+                              onPressed: () {
+                                studentCollectionRef
+                                    .doc(studentList[index].uniqueId)
+                                    .delete();
+                              },
+                              icon: const Icon(Icons.delete),
+                            ),
+                          ],
+                        ),
                       );
                     });
               }
@@ -84,12 +112,51 @@ class _StudentListState extends State<StudentList> {
       ),
     );
   }
+
+  void showAddStudentBottomSheet() {
+    showModalBottomSheet(
+        context: context,
+        builder: (context) {
+          return Column(
+            children: [
+              TextField(
+                controller: nameTEController,
+              ),
+              TextField(
+                controller: idTEController,
+              ),
+              TextField(
+                controller: fieldTEController,
+              ),
+              ElevatedButton(
+                  onPressed: () async {
+                    await studentCollectionRef.add({
+                      'name': nameTEController.text.trim(),
+                      'id': idTEController.text.trim(),
+                      'field': fieldTEController.text.trim(),
+                    });
+                    nameTEController.clear();
+                    idTEController.clear();
+                    fieldTEController.clear();
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Add'))
+            ],
+          );
+        });
+  }
 }
 
 class Student {
   final String name;
   final String id;
   final String field;
+  final String uniqueId;
 
-  Student({required this.name, required this.id, required this.field});
+  Student({
+    required this.name,
+    required this.id,
+    required this.field,
+    required this.uniqueId,
+  });
 }
